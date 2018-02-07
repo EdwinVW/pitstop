fd5984863eea0d72ee99850399cca8cec5678f2d
Edwin van Wijk <edwinvw@live.nl>
2018-02-07 13:21:25 +0100

    Added RunAll scripts to the solution + Added image names to the docker-compose.yml file + fixed syntax-issue in the RunAll powershell script

--- src/RunAllDockerImages.ps1
+++ src/RunAllDockerImages.ps1
@@ -2,4 +2,4 @@
 # running in the foreground. When pressing crtl-c, youre containers will continue to run."
 # Updated something? Then run the RebuildAllDockerImages script to run changes."
 
-docker-compose up -d && docker-compose logs -f
+docker-compose up -d; docker-compose logs -f
