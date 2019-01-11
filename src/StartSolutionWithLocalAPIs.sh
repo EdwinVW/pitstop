7fb01fd21afe8e1d4e5baf7c99875178b6268565
EdwinVW <edwinvw@gmail.com>
2019-01-11 08:08:07 +0100

    Added start-script for running with APIs accessible to localhost

---
+++ src/StartSolutionWithLocalAPIs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Run all containers and publish all APIs available via localhost. 
+# Updated something? Then run the RebuildAllDockerImages script to run changes.
+
+docker-compose -f .\docker-compose.yml -f .\docker-compose.local.yml up
