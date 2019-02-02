d5ed27dac0a45b39383178d0f2d60dc38a399a16
EdwinVW <edwinvw@gmail.com>
2019-02-02 16:33:17 +0100

    Added Istio scripts

--- src/k8s/stop-all.sh
+++ src/k8s/stop-all.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-kubectl delete svc -l system=pitstop -n pitstop
-kubectl delete deploy -l system=pitstop -n pitstop
+kubectl delete svc --all -n pitstop
+kubectl delete deploy --all -n pitstop
+kubectl delete virtualservice --all -n pitstop
+kubectl delete destinationrule --all -n pitstop
