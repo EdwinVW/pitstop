a05dd18d75b1392aa35a1007fd93c901367ec785
EdwinVW <edwinvw@gmail.com>
2019-02-22 06:48:37 +0100

    Removed Kiali scripts

--- src/k8s/Kiali/create-kiali-secret.ps1
+++
@@ -1,8 +0,0 @@
-[string]$user="kialiuser"
-[string]$pass="kialipassword"
-
-$userb64=[Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($user))
-$passb64=[Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($pass))
-
-Write-Host $userb64
-Write-Host $passb64
