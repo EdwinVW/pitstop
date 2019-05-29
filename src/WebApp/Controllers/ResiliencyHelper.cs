2abe6933406c2e88f71fa16b4e1ed6dfab328957
EdwinVW <edwinvw@gmail.com>
2019-05-29 18:13:04 +0200

    Added logging to resiliency helper

--- src/WebApp/Controllers/ResiliencyHelper.cs
+++ src/WebApp/Controllers/ResiliencyHelper.cs
@@ -20,14 +20,18 @@ namespace PitStop.Controllers
         public  async Task<IActionResult> ExecuteResilient(Func<Task<IActionResult>> action, IActionResult fallbackResult)
         {
             var retryPolicy = Policy
-                .Handle<Exception>()
+                .Handle<Exception>((ex) => 
+                    { 
+                        _logger.LogWarning($"Error occured during request-execution. Polly will retry. Exception: {ex.Message}"); 
+                        return true; 
+                    })
                 .RetryAsync(5);
 
             var fallbackPolicy = Policy<IActionResult>
                 .Handle<Exception>()
                 .FallbackAsync(
                     fallbackResult,
-                    (e, c) => Task.Run(() => _logger.LogError(e.Exception.ToString())));
+                    (e, c) => Task.Run(() => _logger.LogError($"Error occured during request-execution. Polly will fallback. Exception: {e.Exception.ToString()}")));
 
             return await fallbackPolicy
                 .WrapAsync(retryPolicy)
