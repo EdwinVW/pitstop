45c6d9f848cfa248224a9f9314aabc990bd1f17e
EdwinVW <edwinvw@gmail.com>
2019-09-08 06:36:41 +0200

    Added script for pushing all pitstop images

---
+++ src/push-images.ps1
@@ -0,0 +1,11 @@
+docker push pitstop/customermanagementapi:latest
+docker push pitstop/customermanagementapi:v1 
+docker push pitstop/customermanagementapi/v2
+docker push pitstop/webapp:latest
+docker push pitstop/workshopmanagementeventhandler:latest
+docker push pitstop/timeservice:latest
+docker push pitstop/notificationservice:latest
+docker push pitstop/invoiceservice:latest
+docker push pitstop/auditlogservice:latest
+docker push pitstop/workshopmanagementapi:latest
+docker push pitstop/vehiclemanagementapi:latest
