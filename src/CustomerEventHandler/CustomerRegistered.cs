dedf81cbd3786bd14b82c673e6b53e9828744822
Ruan Schoombee <Ruan.Schoombee@infosupport.com>
2019-11-13 09:51:11 +0100

    Modified customer registered event class

--- src/CustomerEventHandler/CustomerRegistered.cs
+++ src/CustomerEventHandler/CustomerRegistered.cs
@@ -6,11 +6,13 @@ namespace CustomerEventHandler
 {
     public class CustomerRegistered : Event
     {
-        public Guid customerId { get; set; }  
+        public string CustomerId { get; set; }  
         public string Name { get; set; }   
-        public CustomerRegistered()
+
+        public CustomerRegistered(Guid messageId, string customerId, string name) : base(messageId)
         {
-            
+            CustomerId = customerId;
+            Name = name;
         }
     }
 }
