3d82b358191d13951ab1c8a60bc56ea689464225
Ruan Schoombee <Ruan.Schoombee@infosupport.com>
2019-11-13 09:50:23 +0100

    Added CustomerManager MessageHandler Implementation

--- src/CustomerEventHandler/CustomerManager.cs
+++ src/CustomerEventHandler/CustomerManager.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Linq;
 using Pitstop.Infrastructure.Messaging;
+using Serilog;
 
 namespace CustomerEventHandler
 {
@@ -28,7 +31,29 @@ namespace CustomerEventHandler
 
         public Task<bool> HandleMessageAsync(string messageType, string message)
         {
-            return Task.FromResult(false);
+            var messageObject = MessageSerializer.Deserialize(message);
+            CustomerRegistered customerRegisteredEvent = null;
+
+            try
+            {
+                customerRegisteredEvent = messageObject.ToObject<CustomerRegistered>();
+            }
+            catch (System.Exception ex)
+            {
+                string messageId = messageObject.Property("MessageId") != null ? messageObject.Property("MessageId").Value<string>() : "[unknown]";
+                Log.Error(ex, "Error while handling {MessageType} message with id {MessageId}.", messageType, messageId);
+            }
+
+            if (customerRegisteredEvent != null)
+            {
+                var logMessage = $"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:ffffff")}|";
+                logMessage += $"User has been registered: {customerRegisteredEvent.CustomerId}:{customerRegisteredEvent.Name}{Environment.NewLine}";
+                Console.WriteLine($"{logMessage}");
+                Log.Information("{MessageType} - {Body}", messageType, message);
+            }
+
+            // ack message - try catch deals with errors
+            return Task.FromResult(true);
         }
     }
 }
