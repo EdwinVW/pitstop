3f057be095322ecec861f641243295bb5431f715
Ruan Schoombee <Ruan.Schoombee@infosupport.com>
2019-11-13 09:53:07 +0100

    Modified Program.cs to be the composition root to this microservice.

--- src/CustomerEventHandler/Program.cs
+++ src/CustomerEventHandler/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Pitstop.Infrastructure.Messaging;
+using Serilog;
 
 namespace CustomerEventHandler
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = CreateHostBuilder(args).Build();
+            await host.RunAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var hostBuilder = Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("hostsettings.json", optional: true);
+                    configHost.AddJsonFile($"appsettings.json", optional: false);
+                    configHost.AddEnvironmentVariables();
+                    configHost.AddEnvironmentVariables("PITSTOP_");
+                    configHost.AddCommandLine(args);
+                })
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    config.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: false);
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddTransient<IMessageHandler>((svc) =>
+                    {
+                        var rabbitMQConfigSection = hostContext.Configuration.GetSection("RabbitMQ");
+                        string rabbitMQHost = rabbitMQConfigSection["Host"];
+                        string rabbitMQUserName = rabbitMQConfigSection["UserName"];
+                        string rabbitMQPassword = rabbitMQConfigSection["Password"];
+                        return new RabbitMQMessageHandler(rabbitMQHost, rabbitMQUserName, rabbitMQPassword, "Pitstop", "CustomerEvents", ""); ;
+                    });
+
+                    services.AddHostedService<CustomerManager>();
+                })
+                .UseSerilog((hostContext, loggerConfiguration) =>
+                {
+                    loggerConfiguration.ReadFrom.Configuration(hostContext.Configuration);
+                })
+                .UseConsoleLifetime();
+                
+            return hostBuilder;
         }
     }
 }
