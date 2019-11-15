fc1b606f4336dbaa63ab04ecd86e801c381371ee
Ruan Schoombee <Ruan.Schoombee@infosupport.com>
2019-11-15 10:18:30 +0100

    Added new CustomerUpdated event

---
+++ src/NotificationService/Events/CustomerUpdated.cs
@@ -0,0 +1,22 @@
+﻿using Pitstop.Infrastructure.Messaging;
+using System;
+
+namespace Pitstop.NotificationService.Events
+{
+    public class CustomerUpdated : Event
+    {
+        public readonly string CustomerId;
+        public readonly string Name;
+        public readonly string TelephoneNumber;
+        public readonly string EmailAddress;
+
+        public CustomerUpdated(Guid messageId, string customerId, string name, string telephoneNumber, string emailAddress) :
+            base(messageId)
+        {
+            CustomerId = customerId;
+            Name = name;
+            TelephoneNumber = telephoneNumber;
+            EmailAddress = emailAddress;
+        }
+    }
+}
