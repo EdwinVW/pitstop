e40ede4bd4badbfb5ab5650dd4894936c5acc6d7
Ruan Schoombee <Ruan.Schoombee@infosupport.com>
2019-11-15 08:22:58 +0100

    Modified Mappers class to include Update mappings

--- src/CustomerManagementAPI/Mappers/Mappers.cs
+++ src/CustomerManagementAPI/Mappers/Mappers.cs
@@ -18,7 +18,30 @@ namespace Pitstop.CustomerManagementAPI.Mappers
             command.EmailAddress
         );
 
+        public static CustomerUpdated MapToCustomerUpdated(this UpdateCustomer command) => new CustomerUpdated
+        (
+            System.Guid.NewGuid(),
+            command.CustomerId,
+            command.Name,
+            command.Address,
+            command.PostalCode,
+            command.City,
+            command.TelephoneNumber,
+            command.EmailAddress
+        );
+
         public static Customer MapToCustomer(this RegisterCustomer command) => new Customer
+        {
+            CustomerId = command.CustomerId,
+            Name = command.Name,
+            Address = command.Address,
+            PostalCode = command.PostalCode,
+            City = command.City,
+            TelephoneNumber = command.TelephoneNumber,
+            EmailAddress = command.EmailAddress
+        };
+
+        public static Customer MapToCustomer(this UpdateCustomer command) => new Customer
         {
             CustomerId = command.CustomerId,
             Name = command.Name,
