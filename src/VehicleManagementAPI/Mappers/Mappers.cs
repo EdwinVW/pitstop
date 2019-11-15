f718b6df5a846463bee4c72ba84196e9d1e1c617
Ruan Schoombee <Ruan.Schoombee@infosupport.com>
2019-11-15 09:10:36 +0100

    Modified Mapper to include Update mappings

--- src/VehicleManagementAPI/Mappers/Mappers.cs
+++ src/VehicleManagementAPI/Mappers/Mappers.cs
@@ -1,10 +1,13 @@
 using Pitstop.Application.VehicleManagement.Commands;
+using Pitstop.Application.VehicleManagement.Events;
 using Pitstop.Application.VehicleManagement.Model;
+using System;
 
 namespace Pitstop.VehicleManagementAPI.Mappers
 {
-    public static class Mappers
-    {
+    public static class Mappers 
+    { 
+
         public static Vehicle MapToVehicle(this RegisterVehicle command) => new Vehicle
         {
             LicenseNumber = command.LicenseNumber,
@@ -12,5 +15,34 @@ namespace Pitstop.VehicleManagementAPI.Mappers
             Type = command.Type,
             OwnerId = command.OwnerId
         };
+
+        public static Vehicle MapToVehicle(this UpdateVehicle command) => new Vehicle
+        {
+            LicenseNumber = command.LicenseNumber,
+            Brand = command.Brand,
+            Type = command.Type,
+            OwnerId = command.OwnerId
+        };
+        public static VehicleRegistered MapToVehicleRegistered(RegisterVehicle command)
+        {
+            return new VehicleRegistered(
+                Guid.NewGuid(),
+                command.LicenseNumber,
+                command.Brand,
+                command.Type,
+                command.OwnerId
+            );
+        }
+
+        public static VehicleUpdated MapToVehicleUpdated(UpdateVehicle command)
+        {
+            return new VehicleUpdated(
+                Guid.NewGuid(),
+                command.LicenseNumber,
+                command.Brand,
+                command.Type,
+                command.OwnerId
+            );
+        }
     }
 }
