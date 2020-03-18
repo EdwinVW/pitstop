6d9276509811c79871dd373eeda3a9a89c824ee8
EdwinVW <edwinvw@gmail.com>
2020-03-18 12:04:11 +0100

    Renamed variables (Dutch -> English)

--- src/TestUtils/TestDataGenerators.cs
+++ src/TestUtils/TestDataGenerators.cs
@@ -13,42 +13,42 @@ namespace TestUtils
         public static string GenerateRandomLicenseNumber()
         {
             int type = _rnd.Next(1, 9);
-            string kenteken = null;
+            string licenseNumber = null;
             switch (type)
             {
                 case 1: // 99-AA-99
-                    kenteken = string.Format("{0:00}-{1}-{2:00}", _rnd.Next(1, 99), GenerateRandomCharacters(2), _rnd.Next(1, 99));
+                    licenseNumber = string.Format("{0:00}-{1}-{2:00}", _rnd.Next(1, 99), GenerateRandomCharacters(2), _rnd.Next(1, 99));
                     break;
                 case 2: // AA-99-AA
-                    kenteken = string.Format("{0}-{1:00}-{2}", GenerateRandomCharacters(2), _rnd.Next(1, 99), GenerateRandomCharacters(2));
+                    licenseNumber = string.Format("{0}-{1:00}-{2}", GenerateRandomCharacters(2), _rnd.Next(1, 99), GenerateRandomCharacters(2));
                     break;
                 case 3: // AA-AA-99
-                    kenteken = string.Format("{0}-{1}-{2:00}", GenerateRandomCharacters(2), GenerateRandomCharacters(2), _rnd.Next(1, 99));
+                    licenseNumber = string.Format("{0}-{1}-{2:00}", GenerateRandomCharacters(2), GenerateRandomCharacters(2), _rnd.Next(1, 99));
                     break;
                 case 4: // 99-AA-AA
-                    kenteken = string.Format("{0:00}-{1}-{2}", _rnd.Next(1, 99), GenerateRandomCharacters(2), GenerateRandomCharacters(2));
+                    licenseNumber = string.Format("{0:00}-{1}-{2}", _rnd.Next(1, 99), GenerateRandomCharacters(2), GenerateRandomCharacters(2));
                     break;
                 case 5: // 99-AAA-9
-                    kenteken = string.Format("{0:00}-{1}-{2}", _rnd.Next(1, 99), GenerateRandomCharacters(3), _rnd.Next(1, 10));
+                    licenseNumber = string.Format("{0:00}-{1}-{2}", _rnd.Next(1, 99), GenerateRandomCharacters(3), _rnd.Next(1, 10));
                     break;
                 case 6: // 9-AAA-99
-                    kenteken = string.Format("{0}-{1}-{2:00}", _rnd.Next(1, 9), GenerateRandomCharacters(3), _rnd.Next(1, 10));
+                    licenseNumber = string.Format("{0}-{1}-{2:00}", _rnd.Next(1, 9), GenerateRandomCharacters(3), _rnd.Next(1, 10));
                     break;
                 case 7: // AA-999-A
-                    kenteken = string.Format("{0}-{1:000}-{2}", GenerateRandomCharacters(2), _rnd.Next(1, 999), GenerateRandomCharacters(1));
+                    licenseNumber = string.Format("{0}-{1:000}-{2}", GenerateRandomCharacters(2), _rnd.Next(1, 999), GenerateRandomCharacters(1));
                     break;
                 case 8: // A-999-AA
-                    kenteken = string.Format("{0}-{1:000}-{2}", GenerateRandomCharacters(1), _rnd.Next(1, 999), GenerateRandomCharacters(2));
+                    licenseNumber = string.Format("{0}-{1:000}-{2}", GenerateRandomCharacters(1), _rnd.Next(1, 999), GenerateRandomCharacters(2));
                     break;
             }
 
-            return kenteken;
+            return licenseNumber;
         }
 
-        private static string GenerateRandomCharacters(int aantal)
+        private static string GenerateRandomCharacters(int amount)
         {
-            char[] chars = new char[aantal];
-            for (int i = 0; i < aantal; i++)
+            char[] chars = new char[amount];
+            for (int i = 0; i < amount; i++)
             {
                 chars[i] = _validLicenseNumberChars[_rnd.Next(_validLicenseNumberChars.Length - 1)];
             }
