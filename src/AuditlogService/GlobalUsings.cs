0162798ca95176b23333be382ae1517cd9c8fa02
EdwinVW <edwinvw@gmail.com>
2022-01-23 15:04:13 +0100

    Upgrade AuditLog service to .NET 6

---
+++ src/AuditlogService/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using Pitstop.AuditlogService;
+global using Pitstop.Infrastructure.Messaging;
+global using Pitstop.Infrastructure.Messaging.Configuration;
+global using Serilog;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.DependencyInjection;
