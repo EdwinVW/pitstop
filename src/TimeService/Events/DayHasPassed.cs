7bca053147e677e90d0166954ab443b8bfc6432c
EdwinVW <edwinvw@gmail.com>
2022-01-23 15:05:41 +0100

    Upgrade Time service to .NET 6

--- src/TimeService/Events/DayHasPassed.cs
+++ src/TimeService/Events/DayHasPassed.cs
@@ -1,14 +1,8 @@
-﻿using Pitstop.Infrastructure.Messaging;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Pitstop.TimeService.Events;
 
-namespace Pitstop.TimeService.Events
+public class DayHasPassed : Event
 {
-    public class DayHasPassed : Event
+    public DayHasPassed(Guid messageId) : base(messageId)
     {
-        public DayHasPassed(Guid messageId) : base(messageId)
-        {
-        }
     }
 }
---
+++ src/TimeService/TimeWorker.cs
@@ -0,0 +1,44 @@
+﻿namespace Pitstop.TimeService;
+
+public class TimeWorker : IHostedService
+{
+    DateTime _lastCheck;
+    CancellationTokenSource _cancellationTokenSource;
+    Task _task;
+    IMessagePublisher _messagePublisher;
+
+    public TimeWorker(IMessagePublisher messagePublisher)
+    {
+        _cancellationTokenSource = new CancellationTokenSource();
+        _lastCheck = DateTime.Now;
+        _messagePublisher = messagePublisher;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _task = Task.Run(() => Worker(), _cancellationTokenSource.Token);
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _cancellationTokenSource.Cancel();
+        return Task.CompletedTask;
+    }
+
+    private async void Worker()
+    {
+        while (true)
+        {
+            if (DateTime.Now.Subtract(_lastCheck).Days > 0)
+            {
+                Log.Information($"Day has passed!");
+                _lastCheck = DateTime.Now;
+                DateTime passedDay = _lastCheck.AddDays(-1);
+                DayHasPassed e = new DayHasPassed(Guid.NewGuid());
+                await _messagePublisher.PublishMessageAsync(e.MessageType, e, "");
+            }
+            Thread.Sleep(10000);
+        }
+    }
+}
