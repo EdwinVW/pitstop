ca30a70e846e5c51a1e6c26baecd329f0e2f84f4
Mike van Egmond <38584349+mve@users.noreply.github.com>
2022-10-05 08:56:15 +0200

    Added support for ARM devices. (#120)
    * Added workdir /app to all dockerfiles + small changes
    
    * Added a --no-cache flag for the rebuildallimages script.
    * Updated the package-lock of the webapp.
    * Changed the way node.js is installed in the webapp to also support arm devices.
    * Changed the port of the webapp to 7005 instead of 7000 because 7000 is taken on MacOS.
    
    * Made start and stop scripts executable.
    
    * Update all webapp ports to 7005
    
    Co-authored-by: Edwin van Wijk <edwinvw@gmail.com>

--- src/UITest/TestConstants.cs
+++ src/UITest/TestConstants.cs
@@ -2,5 +2,5 @@ namespace Pitstop.UITest;
 
 public static class TestConstants
 {
-    public static Uri PitstopStartUrl => new Uri("http://localhost:7000");
+    public static Uri PitstopStartUrl => new Uri("http://localhost:7005");
 }
