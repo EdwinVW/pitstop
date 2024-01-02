dd714b29b54b624bcf072ec5a1fd3c4e40fe3d9c
Willem Meints <1550763+wmeints@users.noreply.github.com>
2024-01-02 06:00:06 +0100

    Add AppHost project

---
+++ src/AppHost/Program.cs
@@ -0,0 +1,14 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+
+builder.AddProject<Projects.Pitstop_AuditlogService>("auditlogservice");
+builder.AddProject<Projects.Pitstop_CustomerManagementAPI>("customermanagementapi");
+builder.AddProject<Projects.Pitstop_InvoiceService>("invoiceservice");
+builder.AddProject<Projects.Pitstop_NotificationService>("notificationservice");
+builder.AddProject<Projects.Pitstop_TimeService>("timerservice");
+builder.AddProject<Projects.Pitstop_VehicleManagementAPI>("vehiclemanagementapi");
+builder.AddProject<Projects.Pitstop_WebApp>("webapp");
+builder.AddProject<Projects.Pitstop_WorkshopManagementAPI>("workshopmanagementapi");
+builder.AddProject<Projects.Pitstop_WorkshopManagementEventHandler>("workshopmanagementeventhandler");
+
+builder.Build().Run();
