44ff9507926d77d1f1c512dbd9947834eb07105a
Willem Meints <1550763+wmeints@users.noreply.github.com>
2024-01-02 06:54:20 +0100

    Add service defaults
    Configures logging automatically when used in services.

---
+++ src/ServiceDefaults/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace ServiceDefaults;
+
+public static class Extensions
+{
+    public static IHostApplicationBuilder AddServiceDefaults(this IHostApplicationBuilder builder)
+    {
+        builder.Services.AddSerilog(configure =>
+        {
+            configure.ReadFrom.Configuration(builder.Configuration).Enrich.WithMachineName();
+        });
+        
+        return builder;
+    }
+}
