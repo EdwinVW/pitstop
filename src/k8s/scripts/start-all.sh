ddf797b7735afdea14728c148774b4646b4b2b3c
Stijn Bannink <stijnbannink40@gmail.com>
2024-10-14 09:52:46 +0200

    Replaced and by && in start-all.sh script

--- src/k8s/scripts/start-all.sh
+++ src/k8s/scripts/start-all.sh
@@ -6,7 +6,7 @@
 
 MESHPOSTFIX=''
 
-if [ "$1" != "--nomesh" and  "$1" != "--istio" and "$1" != "--linkerd" ]
+if [ "$1" != "--nomesh" ] && [ "$1" != "--istio" ] && [ "$1" != "--linkerd" ]
 then
     echo "Error: You must specify how to start Pitstop:"
     echo "  start-all.ps1 < --nomesh | --istio | --linkerd >."
