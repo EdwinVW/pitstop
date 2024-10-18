60a2492f9772cff3d8680fb1709b2af97c4559b9
JunjieJuan <junjiejuan@hotmail.com>
2024-10-18 14:41:25 +0200

    tijdelijek commit moet rebase van branch 29

--- src/NotificationService/Events/MaintenanceJobNotification.cs
+++ src/NotificationService/Events/MaintenanceJobNotification.cs
@@ -2,11 +2,16 @@
 
 public class MaintenanceJobNotification : Event
 {
+    public readonly string JobId; 
+    public readonly DateTime StartTime;
+    public readonly DateTime EndTime;
     
-    
-    public MaintenanceJobNotification(Guid messageId, string jobId) :
+    public MaintenanceJobNotification(Guid messageId, string jobId, DateTime startTime, DateTime endTime) :
         base(messageId)
     {
         JobId = jobId;
+        JobId = jobId;
+        StartTime = startTime;
+        EndTime = endTime;
     }
 }
