a771cc760a3a3072514f2fe8e8d2f48677b4456e
Tim van de Ven <tim.vandeven@afas.nl>
2024-10-28 16:31:04 +0100

    Voeg customersupportapi toe aan docker push scripts

--- src/push-images.ps1
+++ src/push-images.ps1
@@ -8,7 +8,8 @@ docker tag pitstop/invoiceservice:1.0 ghcr.io/hanaim-devops/pitstop-team-luna/in
 docker tag pitstop/auditlogservice:1.0 ghcr.io/hanaim-devops/pitstop-team-luna/auditlogservice:1.0
 docker tag pitstop/workshopmanagementapi:1.0 ghcr.io/hanaim-devops/pitstop-team-luna/workshopmanagementapi:1.0
 docker tag pitstop/vehiclemanagementapi:1.0 ghcr.io/hanaim-devops/pitstop-team-luna/vehiclemanagementapi:1.0
-docker tag pitstop/vehiclemanagementapi:1.0 ghcr.io/hanaim-devops/pitstop-team-luna/repairmanagementapi:1.0
+docker tag pitstop/repairmanagementapi:1.0 ghcr.io/hanaim-devops/pitstop-team-luna/repairmanagementapi:1.0
+docker tag pitstop/customersupportapi:1.0 ghcr.io/hanaim-devops/pitstop-team-luna/customersupportapi:1.0
 
 # Push all the tagged images to the same repo with different tags
 docker push ghcr.io/hanaim-devops/pitstop-team-luna/customermanagementapi:1.0
@@ -21,3 +22,4 @@ docker push ghcr.io/hanaim-devops/pitstop-team-luna/auditlogservice:1.0
 docker push ghcr.io/hanaim-devops/pitstop-team-luna/workshopmanagementapi:1.0
 docker push ghcr.io/hanaim-devops/pitstop-team-luna/vehiclemanagementapi:1.0
 docker push ghcr.io/hanaim-devops/pitstop-team-luna/repairmanagementapi:1.0
+docker push ghcr.io/hanaim-devops/pitstop-team-luna/customersupportapi:1.0
