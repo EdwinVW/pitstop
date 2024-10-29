6b6d950393b6cf59627432dab67b4b864f84e2be
Hugo Kemme <hjr.kemme@student.han.nl>
2024-10-29 15:07:31 +0100

    missing service

--- src/push-images.ps1
+++ src/push-images.ps1
@@ -8,6 +8,7 @@ docker tag pitstop/invoiceservice:1.0 ghcr.io/hanaim-devops/pitstop-team-luna/in
 docker tag pitstop/auditlogservice:1.0 ghcr.io/hanaim-devops/pitstop-team-luna/auditlogservice:1.0
 docker tag pitstop/workshopmanagementapi:1.0 ghcr.io/hanaim-devops/pitstop-team-luna/workshopmanagementapi:1.0
 docker tag pitstop/vehiclemanagementapi:1.0 ghcr.io/hanaim-devops/pitstop-team-luna/vehiclemanagementapi:1.0
+docker tag pitstop/vehiclemanagementapi:1.0 ghcr.io/hanaim-devops/pitstop-team-luna/repairmanagementapi:1.0
 
 # Push all the tagged images to the same repo with different tags
 docker push ghcr.io/hanaim-devops/pitstop-team-luna/customermanagementapi:1.0
@@ -19,3 +20,4 @@ docker push ghcr.io/hanaim-devops/pitstop-team-luna/invoiceservice:1.0
 docker push ghcr.io/hanaim-devops/pitstop-team-luna/auditlogservice:1.0
 docker push ghcr.io/hanaim-devops/pitstop-team-luna/workshopmanagementapi:1.0
 docker push ghcr.io/hanaim-devops/pitstop-team-luna/vehiclemanagementapi:1.0
+docker push ghcr.io/hanaim-devops/pitstop-team-luna/repairmanagementapi:1.0
